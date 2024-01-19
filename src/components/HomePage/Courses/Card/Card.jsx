e15bea4ca2779d656beb3145f5c23a10526fb4f3
Bzhezinskyi <bzhezinskyiillia@gmail.com>
2024-01-19 13:24:05 +0200

    Update CurseCard

--- src/components/HomePage/Courses/Card/Card.jsx
+++ src/components/HomePage/Courses/Card/Card.jsx
@@ -1,17 +1,25 @@
 import * as SC from './Card.styled';
 
-export default function Card({ details: { name, txt, duration, price, id } }) {
+export default function Card({
+  details: {
+    title: { type, name },
+    content: { description },
+    duration,
+    price,
+    id,
+  },
+}) {
   return (
     <SC.Container>
+      <SC.Title>{type}</SC.Title>
       <SC.Title>{name}</SC.Title>
-      <SC.Txt>{txt}</SC.Txt>
+      <SC.Txt>{description}</SC.Txt>
       <br />
       <SC.Box>
         <SC.Txt>
-          Тривалість курсу: {duration} <br />
-          Вартість: {price}
+          {duration} <br />
+          {price}
         </SC.Txt>
-        <SC.Txt></SC.Txt>
         <SC.Btn to={`course/${id}`}>Про курс</SC.Btn>
       </SC.Box>
     </SC.Container>
